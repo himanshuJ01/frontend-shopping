bfd1413b299527340794288fca0c8ff49381b7d3
Himanshu Janbandhu <h.janbandhu@outlook.com>
2021-05-20 20:58:14 +0530

    essential changes 01

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 //const baseUrl = "https://flipkart-clone.herokuapp.com";
-const baseUrl = "http://localhost:2000"||"https://filpkart-clone.herokuapp.com";
+const baseUrl = "https://filpkart-clone.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
